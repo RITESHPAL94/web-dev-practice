0ef4b5be500f76b326f8387cec9a4ee78efc5f29
RITESHPAL94 <riteshpal9051@gmail.com>
2026-08-28 04:54:59 +0530

    Javascript Variables

---
+++ JAVASCRIPT/CHAPTER 02/script.js
@@ -0,0 +1,50 @@
+// var a = 5;
+// var b = 6;
+// var c = "ritesh"
+// var _a = "ritesh";
+// var 55a = "rahul" Not Allowed
+// console.log(a+b+8)
+// console.log(typeof a,typeof b,typeof c);
+
+
+// const a1 = 6;
+// a1 = a1 +1;
+// Not Allowed because a1 is constant
+
+let a = 5;
+let b = 6;
+let c = "ritesh"
+let _a = "ritesh";
+
+{
+    let a = 66
+    console.log(a)
+}
+console.log(a)
+
+
+let x = "rahul"
+let y = 22
+let z = 3.55
+const p = true
+let q = undefined
+let r = null
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o = {
+    name: "rahul",
+    "job code":8900,
+    "is_handsome":true
+}
+
+console.log(o)
+
+o.salary = "100crores"
+
+console.log(o)
+
+o.salary = "500crores"
+
+console.log(o)
