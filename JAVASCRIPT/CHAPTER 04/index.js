7608669851e48c2b73b75b798193bcddab8816ec
RITESHPAL94 <riteshpal9051@gmail.com>
2026-08-30 02:26:37 +0530

    Loops

---
+++ JAVASCRIPT/CHAPTER 04/index.js
@@ -0,0 +1,39 @@
+console.log("I am tutorial on Loops")
+
+// let a = 1
+// console.log(a)
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i)
+    
+// }
+
+// let obj = {
+//     name: "Ritesh",
+//     role: "Programmer",
+//     company: "Ritesh AI"
+// }
+
+// for (const key in obj) {
+//     if (!Object.hasOwn(obj, key)) continue;
+    
+//     const element = obj[key];
+    
+//     console.log(key,element)
+// }
+
+// for (const c of "Ritesh") {
+//     console.log(c)
+// }
+
+// i = 0
+// while (i<6){
+//     console.log(i)
+//     i++
+// }    
+
+i = 10
+do {
+    console.log(i)
+    i++
+} while (i<6);
