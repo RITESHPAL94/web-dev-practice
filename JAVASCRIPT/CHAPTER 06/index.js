3ea07a2aa09ce1a231ef5c3eb8de5cd6200eb311
RITESHPAL94 <riteshpal9051@gmail.com>
2026-09-08 20:13:53 +0530

    Javascript Strings

---
+++ JAVASCRIPT/CHAPTER 06/index.js
@@ -0,0 +1,28 @@
+// Javascript Strings 
+
+let name = "ritesh"
+console.log(name)
+console.log(name[0])
+console.log(name[1])
+console.log(name[2])
+console.log(name[3])
+console.log(name[4])
+console.log(name[5])
+// console.log(name[6])
+
+console.log(name.length)
+
+let real_names = "raj"
+let friend = "rajat"
+console.log("His name is "+ real_names + " and his friend name is " +friend)
+
+console.log(`His name is ${name} and his friend name is ${real_names}`)
+
+let b = "rahul"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1))
+console.log(b.replace("hu","7"))
+console.log(b.concat(name,"aarti"))
+console.log(b)
