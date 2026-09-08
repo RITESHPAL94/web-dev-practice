0bc9c1c6b110f7076ca595a2d35e5beb1dba3626
RITESHPAL94 <riteshpal9051@gmail.com>
2026-09-09 01:01:54 +0530

    Javascript Arrays

---
+++ JAVASCRIPT/CHAPTER 07/index.js
@@ -0,0 +1,21 @@
+// Javascript Array 
+
+let arr = [1, 2, 3]
+          0, 1, 2
+
+// console.log(arr , typeof arr)
+// console.log(arr.length)
+arr[0] = 566
+// console.log(arr[0])
+// console.log(arr[2])
+// console.log(arr[1])
+
+console.log(arr.toString())
+console.log(arr.join(" and "))
+
+// console.log(arr.pop())
+console.log(arr.push("ritesh"))
+console.log(arr.shift())
+console.log(arr.unshift("rahul"))
+console.log(arr)
+console.log(arr.sort())
