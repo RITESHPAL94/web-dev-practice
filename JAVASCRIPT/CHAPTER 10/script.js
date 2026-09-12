ed008c95080ff4e4789d0a09fa82e6cd46d032ba
RITESHPAL94 <riteshpal9051@gmail.com>
2026-09-13 01:54:15 +0530

    Javascript - Selecting by IDs, Classes and More

---
+++ JAVASCRIPT/CHAPTER 10/script.js
@@ -0,0 +1,14 @@
+console.log("The GOAT")
+
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes)
+// boxes[2].style.backgroundColor = "red"
+// document.getElementById("red").style.backgroundColor = "red"
+// document.querySelector(".box").style.backgroundColor = "green"
+// document.querySelectorAll(".box").forEach(e=>{
+//     e.style.backgroundColor = "green"
+// })
+document.getElementsByTagName("div")
+
+
+
