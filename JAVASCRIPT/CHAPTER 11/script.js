682ed4fbf57e14337cf9045aab210a6c73ccdb2c
RITESHPAL94 <riteshpal9051@gmail.com>
2026-09-14 03:36:30 +0530

    Insert and Removing Element

---
+++ JAVASCRIPT/CHAPTER 11/script.js
@@ -0,0 +1,20 @@
+// ocument.querySelector(".box").innerHTML = "hey i am ritesh"
+// 'hey i am ritesh'
+// document.querySelector(".box").innerHTML = "hey i am ritesh"
+// 'hey i am ritesh'
+// document.querySelector(".container").hidden = true
+// true
+// document.querySelector(".container").textContent
+// '\n        hey i am ritesh\n    '
+// document.querySelector(".container").nodeName
+// 'DIV'
+// document.querySelector(".container").tagName
+// 'DIV'
+// document.querySelector(".container").outerHTML
+// '<div class="container" hidden="">\n        <div class="box">hey i am ritesh</div>\n    </div>'
+// document.querySelector(".container").innerText
+// '\n        hey i am ritesh\n    '
+// document.querySelector(".container").innerHTML
+// '\n        <div class="box">hey i am ritesh</div>\n  
+// document.getMode = "on"
+// 'on'
